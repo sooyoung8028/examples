0b0438e9f953ebe04a415b8679a1318d7562224c
sooyoung8028 <sooyoung8028@naver.com>
2025-05-21 01:55:00 +0000

    ch4-1:골백 함수 예제(1-1)setInterval

---
+++ 사물인터넷/ch4-1.js
@@ -0,0 +1,10 @@
+var count = 0;
+var timer = setInterval(function() {
+                            function () {
+                            console.log(count);
+                            if (++count > 4) clearInterval(timer);
+                            }
+  console.log(count);
+  if (++count > 4) clearInterval(timer);
+    },300);
+
