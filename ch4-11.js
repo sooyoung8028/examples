2aeec14853f88de7943583be10cc23ea807ff290
sooyoung8028 <sooyoung8028@naver.com>
2025-05-23 06:05:20 +0000

    ch4-11: 콜백 함수 내부의 this에 다른 값을 바인딩하는 방법(2)-bind메서드 활용

---
+++ ch4-11.js
@@ -0,0 +1,14 @@
+var obj1 = {
+    name: "obj1",
+    func: function () {
+        console.log(this.name);
+    },
+};
+setTimeout(obj1.func.bind(obj1), 1000);
+
+var obj2 = {
+    name: "obj2"};
+    setTimeout(obj1.func.bind(obj2), 1500);   function() {
+        console.log(this.name);
+    }
+    
