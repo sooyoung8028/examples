1a19374da9897a4692f4d83a62c853a23079519f
sooyoung8028 <sooyoung8028@naver.com>
2025-06-12 10:18:21 +0000

    ch7-7.js: Rectangle을 상속하는 Sqaure 클래스

---
+++ ch7-7.js
@@ -0,0 +1,14 @@
+var Rectangle = function
+(width, height) {
+    this.width = width; 
+    this.height = height;
+};
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
+var rect = new Rectangle(3,4);
+console.log(rect.getArea());
+var Square = function(width) {
+    Rectangle.call(this, width, width); 
+};
+Square.prototype = new Rectangle();
